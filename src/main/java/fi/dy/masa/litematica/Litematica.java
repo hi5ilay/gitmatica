a0d35dd91f7f2ebc6cc5d2049b3a3b7ea62c0d71
Sakura Ryoko <sakura@ryoko.soy>
2025-07-27 10:36:21 -0400

    Bump malilib

--- src/main/java/fi/dy/masa/litematica/Litematica.java
+++ src/main/java/fi/dy/masa/litematica/Litematica.java
@@ -1,10 +1,12 @@
 package fi.dy.masa.litematica;
 
-import fi.dy.masa.litematica.compat.sodium.SodiumCompat;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
 import net.fabricmc.api.ModInitializer;
+
 import fi.dy.masa.malilib.event.InitializationHandler;
+import fi.dy.masa.malilib.event.RenderEventHandler;
 import fi.dy.masa.litematica.config.Configs;
 
 public class Litematica implements ModInitializer
@@ -15,6 +17,7 @@ public class Litematica implements ModInitializer
     public void onInitialize()
     {
         InitializationHandler.getInstance().registerInitializationHandler(new InitHandler());
+//        RenderEventHandler.getInstance().registerSpecialGuiRenderer();
     }
 
     public static void debugLog(String msg, Object... args)
